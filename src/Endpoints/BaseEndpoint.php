d054ebe2bdb10da9a202b38136e606915e8b6d21
Ronildo-Sousa <Ronildo-Sousa@users.noreply.github.com>
2023-05-08 22:15:04 +0000

    Fix styling

--- src/Endpoints/BaseEndpoint.php
+++ src/Endpoints/BaseEndpoint.php
@@ -1,12 +1,11 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace RonildoSousa\DevtoForLaravel\Endpoints;
 
 use Illuminate\Support\Collection;
 use RonildoSousa\DevtoForLaravel\DevtoForLaravel;
-use RonildoSousa\DevtoForLaravel\Entities\Article;
 
 class BaseEndpoint
 {
