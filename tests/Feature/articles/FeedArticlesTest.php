0beaa12ed8cd451e0522b65326c3eb76a9922a7b
Ronildo Sousa <sousaronildo400@gmail.com>
2023-05-08 11:21:44 -0300

    adds a helper to config files

---
+++ tests/Feature/articles/FeedArticlesTest.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types = 1);
+
+it('test a feature', function () {
+    dd(configFile('teste.emails'));
+});
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,31 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types = 1);
 
 use RonildoSousa\DevtoForLaravel\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+
+function configFile(string $key = null): string|array
+{
+    if (is_null($key)) {
+        return config('devto-for-laravel');
+    }
+
+    $keys        = explode('.', $key);
+    $fileContent = include(getcwd() . '/config/' . $keys[0] . '.php');
+
+    array_shift($keys);
+
+    $tempContent = $fileContent;
+
+    if (count($keys) > 0) {
+        foreach ($keys as $item) {
+            $tempContent = $tempContent[$item];
+        }
+
+        return $tempContent;
+    }
+
+    return $fileContent;
+}
